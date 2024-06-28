eb5a1ba8c2ea5089d73588f10116ce02fef05f7f
Honey -- <hoorulain708@gmail.com>
2024-06-28 21:25:03 +0500

    initial commit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <div className='w-full max-h-screen relative'>
       <Image src={nature} alt='banner image' className='w-full max-h-screen object-contain ' />
-      <div className='absolute top-0 w-full h-full bg-black/30 text-gray-100 flex flex-col items-center justify-center '><h2 className='text-5xl lg:text-[110px] font-bold'>Nature's Narrartive</h2>
+      <div className='absolute top-0 w-full h-full bg-black/30 text-gray-100 flex flex-col items-center justify-center '><h2 className='text-5xl lg:text-[110px] font-bold'>Nature&apos;s Narrartive</h2>
       <p className='text-xl md:text-2xl lg:text-5xl font-semibold'></p>
 
       
